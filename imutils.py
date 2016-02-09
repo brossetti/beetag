c235cd0a468047eaa3738ebc4531ddb3d9b40b79
brossetti <brossetti@nsintheta.com>
2016-02-08 21:52:13 -0500

    added fitrect to imutils.py

--- imutils.py
+++ imutils.py
@@ -3,6 +3,7 @@ Image Utilities
 
 A collection of functions for handling and annotating images
 """
+import numpy as np
 from sys import exc_info
 from PIL import Image
 from matplotlib import pyplot as plt
@@ -60,22 +61,49 @@ class ImageClicker(object):
 
 def fitrect(points):
     '''Finds the minimum bounding rectangle'''
-
     #determine convex hull of points
     hull = ConvexHull(points)
+    pts = hull.points[hull.vertices]
 
     #determine unit edge direction
-    vects = np.difF(x + [x[0]], axis=0)
+    vects = np.diff(np.vstack((pts, pts[0])), axis=0)
     norms = np.linalg.norm(vects, axis=1)
     uvects = np.dot(np.diag(1/norms),vects)
     nvects = np.fliplr(uvects)*(-1,1)
 
     #find MBR
-    
+    minmax = lambda x: np.vstack((x.min(axis=0),x.max(axis=0)))
+    x = minmax(np.dot(pts, np.transpose(uvects)))
+    y = minmax(np.dot(pts, np.transpose(nvects)))
+
+    areas = (y[0,:]-y[1,:])*(x[0,:]-x[1,:])
+    idx = np.argmin(areas)
+
+    #define the rectangle
+    xys = np.column_stack((x[[0,1,1,0,0],idx], y[[0,0,1,1,0],idx]))
+    rect  = np.dot(xys, np.vstack((uvects[idx,:],nvects[idx,:])))
  
+    return rect    
+
 # Testing
 if __name__ == "__main__":
+    import pdb
+    from matplotlib.patches import Polygon
     image = imread('/Users/blair/Desktop/bee/Photos/IMG_0324.JPG')
     clickim = ImageClicker(image, 4)
 
     print clickim.coords
+
+    rect = fitrect(clickim.coords)
+
+    print(rect)
+
+    fig = plt.figure()
+    fig.canvas.set_window_title('Results')
+    plt.axes([0,0,1,1])
+    plt.imshow(clickim.image)
+    plt.axis('image')
+
+    p = Polygon(rect, alpha=0.4)
+    plt.gca().add_artist(p)    
+    plt.show()
