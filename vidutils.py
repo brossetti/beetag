dbd17ca349f291e397e0a21efc9a0771d9e44fcc
brossetti <brossetti@nsintheta.com>
2016-03-10 00:42:52 -0500

    added function to get active region

--- vidutils.py
+++ vidutils.py
@@ -75,9 +75,37 @@ def active_region(video):
     """
 
     # calculate variance over frame dimension
-    varim = np.var(video, axis=2)
+    var_img = np.var(video, axis=2)
+
+    # rescale variance image to 0-255
+    var_img = cv2.normalize(var_img, var_img, 0, 255, cv2.NORM_MINMAX)
 
     # threshold variance image
-    ret, thresh_im = cv2.threshold(varim, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    ret, thresh_img = cv2.threshold(var_img.astype('uint8'), 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    return thresh_im
+    # dilate
+    kernel = np.ones((3,3),np.uint8)
+    thresh_img = cv2.dilate(thresh_img, kernel, iterations = 1)
+
+    # fill holes
+    floodfill = thresh_img.copy()
+    h, w = thresh_img.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+    cv2.floodFill(floodfill, mask, (0,0), 255);
+    thresh_img = thresh_img | cv2.bitwise_not(floodfill)
+
+    # find largest contour
+    cntr_img, contours, hierarchy = cv2.findContours(thresh_img.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cntr_area = [cv2.contourArea(cntr, False) for cntr in contours]
+    cntr_idx = np.argmax(cntr_area)
+
+    # get bounding rectangle
+    bbox = cv2.boxPoints(cv2.boundingRect(contours[cntr_idx]))
+
+    # get minimum area bounding rectangle
+    rect = cv2.minAreaRect(contours[cntr_idx])
+    mbr = cv2.boxPoints(rect)
+    mbr = np.int0(mbr)
+
+
+    return bbox, mbr
